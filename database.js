b47f9ccf42994eb5de07bb2311e85e2c0ccc7ccf
William Parra <w3webservices.net@gmail.com>
2022-02-02 10:29:22 -0400

    database connection function added

--- database.js
+++ database.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+const { database } = require('./config')
+
+const main = async () => {
+    try {
+        await mongoose.connect(database.url, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+
+        console.log("db connected")
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+module.exports = main;
